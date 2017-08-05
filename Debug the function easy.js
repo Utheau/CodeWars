e4688f957a149e0a4d84831414851921bdd0fb85
Antonio Paolo Mecci <antoniopaolomecci@gmail.com>
2017-08-05 14:27:09 +0200

    challenge n78 7kyu JS

---
+++ Debug the function easy.js
@@ -0,0 +1,15 @@
+function multi(arr) {
+  return arr.reduce(function (a, b) {
+    return a * b;
+  });
+}
+
+function add(arr) {
+  return arr.reduce(function (a, b) {
+    return a + b;
+  });
+}
+
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
